797d35051b1f53e556e645d70038bb452860682e
Abdulsomed Jibril <chuchuchacha151@gmail.com>
2026-06-14 11:34:08 +0300

    update install.py 3

--- zkteco_attendance/zkteco_attendance/install.py
+++ zkteco_attendance/zkteco_attendance/install.py
@@ -9,8 +9,6 @@ from frappe import _
 def after_install():
     """Run after app is installed via bench install-app."""
     _create_biometric_device_manager_role()
-    # _add_employee_biometric_field()
-    # _add_employee_checkin_device_field()
     _add_employee_checkin_zk_uid_field()
     _add_employee_checkin_overtime_field()
     frappe.db.commit()
@@ -31,46 +29,6 @@ def _create_biometric_device_manager_role():
             "is_custom": 1,
         })
         role.insert(ignore_permissions=True)
-
-
-# def _add_employee_biometric_field():
-#     """Add attendance_device_id custom field to Employee if it doesn't exist."""
-#     if frappe.get_meta("Employee").has_field("attendance_device_id"):
-#         return
-
-#     cf = frappe.get_doc({
-#         "doctype": "Custom Field",
-#         "dt": "Employee",
-#         "module": "Zkteco Attendance",
-#         "label": "Biometric Attendance ID",
-#         "fieldname": "attendance_device_id",
-#         "fieldtype": "Data",
-#         "insert_after": "employee_number",
-#         "description": "ID enrolled on the ZKTeco biometric device. Used to match attendance records.",
-#         "in_list_view": 0,
-#         "search_index": 1,
-#     })
-#     cf.insert(ignore_permissions=True)
-
-
-# def _add_employee_checkin_device_field():
-    """Add device_id field to Employee Checkin if not already there."""
-    if frappe.db.exists("Custom Field", {"dt": "Employee Checkin", "fieldname": "device_id"}):
-        return
-
-    cf = frappe.get_doc({
-        "doctype": "Custom Field",
-        "dt": "Employee Checkin",
-        "module": "Zkteco Attendance",
-        "label": "Biometric Device",
-        "fieldname": "device_id",
-        "fieldtype": "Data",
-        "insert_after": "log_type",
-        "description": "ZKTeco device that recorded this checkin.",
-        "in_list_view": 0,
-    })
-    cf.insert(ignore_permissions=True)
-
 
 def _add_employee_checkin_zk_uid_field():
     """Add zk_uid field to Employee Checkin (raw device record id, used for de-duplication)."""
